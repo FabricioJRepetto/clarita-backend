80234a27e467c0f8f1cd675ed06c5784f857cafd
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-02-20 12:23:26 -0300

    PORT test

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ import { router as cabinRouter } from "../apiServices/cabin/router.js"
 import { verifyToken } from "../utils/verify.js"
 
 router.get("/", (__, res) => {
-    res.send("Hello World!");
+    res.send("Bienvenido al backend de Cabañas Clarita!");
 });
 router.get('/sensei', (__, res) => {
     res.send('El facusama te bendice.')
