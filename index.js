15abcec5693cd548cad0bde557179404108b5976
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-02-20 12:34:24 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import app from "./src/app.js";
 import { start } from "./src/microservices/DBConnection.js";
-const { PORT } = process.env;
+const PORT = process.env.PORT || 4000;
 
 export const listen = () => {
-    app.listen(PORT || 4000, () => {
-        console.log(`\x1b[32m✔ \x1b[0m · Server listening on port ${PORT || 4000}`);
+    app.listen(PORT, () => {
+        console.log(`\x1b[32m✔ \x1b[0m · Server listening on port ${PORT}`);
     });
 }
 
